f008be8ccd124f5ccb2cab95c4b4c5772a947c37
NAME <hilmiawananggoro189@gmail.com>
2023-03-17 15:43:39 +0700

    praktikum-4

---
+++ pertemuan-4/startbootstrap-sb-admin-2-gh-pages/admin/daftar.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'templates/head.php';
+include_once 'templates/sidebar.php';
+include_once 'templates/topbar.php';
+?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Silahkan isi Form Pendaftaran</h1>
+
+</div>
+<!-- /.container-fluid -->
+<!-- End of Main Content -->
+</div>
+<?php
+include_once 'templates/footer.php';
+?>
