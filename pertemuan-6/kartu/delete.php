3b1f2a4e399f674682f780f003a99839bfc889d8
NAME <hilmiawananggoro189@gmail.com>
2023-03-31 16:06:41 +0700

    pertemuan-6

---
+++ pertemuan-6/kartu/delete.php
@@ -0,0 +1,13 @@
+<!-- 
+Nama : Rizky Hilmiawan Anggoro
+Rombel : TI-10
+NIM : 0110222140 
+-->
+<?php 
+require_once '../dbkoneksi.php';
+$_iddel=$_GET['iddel'];
+$sql="DELETE FROM kartu WHERE id=?";
+$st = $dbh->prepare($sql);
+$st->execute([$_iddel]);
+header('location:index.php');
+?>
